146d1fdf76d7d715fc687c0b619730bc29838854
Sriyam <sriyam2004@gmail.com>
2023-09-15 19:07:11 +0530

    New Commit

---
+++ CircularLinkedList.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+
+struct node
+{
+    int data;
+    node* next;
+};
+node* head = nullptr;
+
+int createLinkedList(node* start)
+{
+    node* ptr, * current;
+    ptr = nullptr;
+    current = nullptr;
+    char ch;
+    int c=1;
+
+    std::cout << "Creating a Linked List now:" << std::endl;
+
+    do
+    {
+        ptr = (node* )malloc(sizeof(node));
+
+        if(ptr == nullptr)
+        {
+            std::cout << "Memory Allocation Failed!";
+            return 0;
+        }
+
+        std::cout << "Enter data: ";
+        std::cin >> ptr->data;
+        ptr->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = ptr;
+            current = ptr;
+        }
+        else
+        {
+            current->next = ptr;
+            current = ptr;
+        }
+
+        std::cout << "Do you want to add more nodes?: (Y/N) ";
+        std::cin >> ch;
+
+        c++;
+    } while (ch == 'y' || ch == 'Y');
+
+    if (current != nullptr)
+    {
+        current->next = head;
+    }
+    
+    return c;
+}
+
+void displayLinkedList(node* start, int number)
+{
+    node* ptr = start;
+    int i=1;
+
+    if (start == nullptr)
+    {
+        std::cout << "The Linked List is empty!";
+        return;
+    }
+    else
+    {
+        while (i < number)
+        {
+            std::cout << ptr->data << " ";
+            ptr = ptr->next;
+            i++;
+        }
+    }
+}
+
+void freeLL()
+{
+    struct node* current = head;
+    struct node* next;
+
+    while (current != nullptr)
+    {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    head = nullptr;
+}
+
+int main()
+{
+    int count;
+
+    count = createLinkedList(head);
+    displayLinkedList(head, count);
+    freeLL();
+
+    return 0;
+}
